9e12c4254de00d881a53e2a8aec3949e4c7423a5
Averianov Yuriy <averianovj@gmail.com>
2022-12-17 19:28:29 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mediasol/openjdk17-slim-jprofiler
 
-EXPOSE 8080
+EXPOSE 8081
 
 COPY target/authorizationService-0.0.1-SNAPSHOT.jar app.jar
 
--- src/test/java/ru/netology/authorizationservice/AuthorizationServiceApplicationTests.java
+++ src/test/java/ru/netology/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -1,13 +1,59 @@
 package ru.netology.authorizationservice;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthorizationServiceApplicationTests {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Container
+    private static final GenericContainer<?> myAppDev = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+
+    @Container
+    private static final GenericContainer<?> myAppProd = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        myAppDev.start();
+        myAppProd.start();
+    }
+
     @Test
-    void contextLoads() {
+    void contextLoadsSuccess() {
+        ResponseEntity<String> forDev = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/authorize?user=Tom&password=123456789", String.class);
+        ResponseEntity<String> forProd = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/authorize?user=Bob&password=qazwsxedcrfv", String.class);
+
+        Assert.assertEquals("[\"WRITE\",\"READ\"]", forDev.getBody());
+        Assert.assertEquals("[\"DELETE\"]", forProd.getBody());
+
+        System.out.println(forDev.getBody());
+        System.out.println(forProd.getBody());
+    }
+
+    @Test
+    void contextLoadsBadRequest() {
+        ResponseEntity<String> forDev = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/authorize?user=Tom&password=123789", String.class);
+        ResponseEntity<String> forProd = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/authorize?user=Bob&password=qazrfv", String.class);
+
+        Assert.assertEquals("Exception: Unknown user Tom", forDev.getBody());
+        Assert.assertEquals("Exception: Unknown user Bob", forProd.getBody());
+
+        System.out.println(forDev.getBody());
+        System.out.println(forProd.getBody());
     }
 
 }
